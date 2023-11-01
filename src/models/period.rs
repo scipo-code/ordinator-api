b34352cb04ef72bba5f2dd9333e621d5a349989c
Mike <109979238+mhkr1898@users.noreply.github.com>
2023-11-01 11:37:37 +0100

    fix: period as string key in RawInputMessage (#10)

--- src/models/period.rs
+++ src/models/period.rs
@@ -1,4 +1,4 @@
-use chrono::{DateTime, Utc, Datelike};
+use chrono::{DateTime, TimeZone, Utc, Datelike};
 use serde::{Deserialize, Serialize};
 use std::fmt::{self, Display};
 
@@ -37,6 +37,34 @@ impl Period {
             if end_date.iso_week().week() == 1 { 52 } else { end_date.iso_week().week() });
         Period { id: id, period_string: period_string, start_date: start_date, end_date: end_date}
     }
+    pub fn new_from_string(period_string: &str) -> Result<Period, &'static str> {
+        // Parse the string
+        let parts: Vec<&str> = period_string.split("-").collect();
+        if parts.len() != 3 {
+            return Err("Invalid period string format");
+        }
+        dbg!(parts.clone());
+        // Parse year and weeks
+        let year = parts[0].parse::<i32>().map_err(|_| "Invalid year")?;
+        let start_week = parts[1][1..2].parse::<u32>().map_err(|_| "Invalid start week")?;
+        let end_week = parts[2].parse::<u32>().map_err(|_| "Invalid end week")?;
+
+        // Convert week number to a DateTime<Utc>
+        let start_date = Utc.isoywd(year, start_week, chrono::Weekday::Mon).and_hms(0, 0, 0);
+        let end_date = if end_week == 52 {
+            Utc.isoywd(year, end_week, chrono::Weekday::Sun).and_hms(23, 59, 59)
+        } else {
+            Utc.isoywd(year, end_week + 1, chrono::Weekday::Mon).and_hms(0, 0, 0) - chrono::Duration::seconds(1)
+        };
+
+        // Create Period
+        Ok(Period {
+            id: 0, // Assuming default value for id, modify as needed
+            period_string: period_string.to_string(),
+            start_date: start_date,
+            end_date: end_date,
+        })
+    }
 }
 
 impl Period {
@@ -62,3 +90,5 @@ impl Display for Period {
         write!(f, "Period: {}", self.period_string)
     }
 }
+
+ 
