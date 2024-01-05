3779046dcefe92b1364e087e0812330b88465b71
Christian Brunbjerg <44016161+brunbjerg@users.noreply.github.com>
2024-01-05 13:24:52 +0100

    Fixing branch (#24)
    * partial implementation | added julia bindings for work_planner agent
    
    * comment out julia code
    
    * test_unschedule_work_order
    
    * partial feat
    
    * testing: fixed unit tests for scheduler algorithm
    
    * changes to CI; only on PR; removal of deny
    
    * testing & CI; added tarpaulin (test coverage)
    
    * refactor: used clippy to refactor whole codebase
    
    * complete pull request fail again
    
    * fixed large pull request
    
    * feat: dev-container added | cargo watch | tests for actor messages
    
    * added tests to excel reading | logging
    
    * fix: unschedule works again
    
    * feat: universally accessible SchedulingEnvironment
    
    * implementation of time_environment message
    
    * refactor & test: scheduler message system & Period::new_from_string
    
    ---------
    
    Co-authored-by: s174460 <s174460@student.dtu.dk>

--- src/agents/scheduler_agent/display.rs
+++ src/agents/scheduler_agent/display.rs
@@ -1,22 +1,22 @@
+use std::collections::hash_map::RandomState;
+use std::collections::HashMap;
 use std::fmt;
 use std::fmt::Display;
-use std::collections::HashMap;
-use std::collections::hash_map::RandomState;
 
 use crate::agents::scheduler_agent::SchedulerAgent;
 
-use super::scheduler_message::{SchedulerRequests, ManualResource, WorkOrderPeriodMapping};
+use super::scheduler_message::{ManualResource, SchedulerRequests, WorkOrderPeriodMapping};
 
 impl Display for SchedulerAgent {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, 
+        write!(
+            f,
             "SchedulerAgent: \n
             Platform: {}, \n
             SchedulerAgentAlgorithm: {:?}, \n
-            WebSocketAgent Addr: {:?}", 
-            self.platform, 
-            self.scheduler_agent_algorithm,
-            self.ws_agent_addr)
+            WebSocketAgent Addr: {:?}",
+            self.platform, self.scheduler_agent_algorithm, self.ws_agent_addr
+        )
     }
 }
 
@@ -25,7 +25,11 @@ pub struct DisplayableManualResource(pub HashMap<(String, String), f64, RandomSt
 impl fmt::Display for DisplayableManualResource {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         for ((resource, period), capacity) in self.0.iter() {
-            writeln!(f, "Resource: {}, {}, Capacity: {}", resource, period, capacity)?;
+            writeln!(
+                f,
+                "Resource: {}, {}, Capacity: {}",
+                resource, period, capacity
+            )?;
         }
         Ok(())
     }
@@ -38,15 +42,23 @@ impl fmt::Display for SchedulerRequests {
                 write!(f, "name: {}", input.name)?;
 
                 for work_order_period_mapping in input.work_order_period_mappings.iter() {
-                    write!(f, "work_order_period_mapping: {}\n", work_order_period_mapping)?;
+                    writeln!(
+                        f,
+                        "work_order_period_mapping: {}",
+                        work_order_period_mapping
+                    )?;
                 }
                 for manual_resource in input.manual_resources.iter() {
-                    write!(f, "manual_resource: {}\n", manual_resource)?;
+                    writeln!(f, "manual_resource: {}", manual_resource)?;
                 }
                 Ok(())
             }
             SchedulerRequests::WorkPlanner(work_planner) => {
                 write!(f, "work_planner: {:?}", work_planner.cannot_schedule)?;
+                Ok(())
+            }
+            SchedulerRequests::Period(period) => {
+                write!(f, "period: {:?}", period)?;
                 Ok(())
             }
         }
@@ -55,12 +67,20 @@ impl fmt::Display for SchedulerRequests {
 
 impl fmt::Display for WorkOrderPeriodMapping {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "work_order: {}, period: {:?}", self.work_order_number, self.period_status)
+        write!(
+            f,
+            "work_order: {}, period: {:?}",
+            self.work_order_number, self.period_status
+        )
     }
 }
 
 impl fmt::Display for ManualResource {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "resource: {}, period: {}, capacity: {}", self.resource, self.period.period_string, self.capacity)
+        write!(
+            f,
+            "resource: {}, period: {}, capacity: {}",
+            self.resource, self.period.period_string, self.capacity
+        )
     }
 }
