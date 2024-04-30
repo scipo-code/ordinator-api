01d2bd4d9afadf924aa2eb31583f3014629eb39a
brunbjerg <brunbjerg1423@live.dk>
2024-04-30 16:35:14 +0200

    added: script structure | started: JSON based API with structs

---
+++ scheduling_system/src/api/strategic_json.rs
@@ -0,0 +1,7 @@
+struct StrategicStatusJson {
+    strategic_objective: u64,
+    number_of_work_orders: u64,
+    number_of_worker_entries: u64,
+    number_of_strategic_periods: u64,
+    number_of_tactical_days: u64,
+}
