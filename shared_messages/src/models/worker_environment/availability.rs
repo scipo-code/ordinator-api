c58bb580ce43e6ad485805b4a90cdcc100a942d9
brunbjerg <brunbjerg1423@live.dk>
2024-05-30 10:19:54 +0200

    change: updated the operational toml specification

--- shared_messages/src/models/worker_environment/availability.rs
+++ shared_messages/src/models/worker_environment/availability.rs
@@ -16,17 +16,23 @@ pub struct TomlAvailability {
     end_date: toml::value::Datetime,
 }
 
-// impl From<TomlAvailability> for Availability {
-//     fn from(value: TomlAvailability) -> Self {
-//         let start_date_time: DateTime<Utc> = value.start_date.to_string();
-//         let end_date_time: DateTime<Utc> = value.end_date.to_string();
+impl From<TomlAvailability> for Availability {
+    fn from(value: TomlAvailability) -> Self {
+        let start_date_time: DateTime<Utc> =
+            DateTime::parse_from_rfc3339(&value.start_date.to_string())
+                .unwrap()
+                .to_utc();
+        let end_date_time: DateTime<Utc> =
+            DateTime::parse_from_rfc3339(&value.end_date.to_string())
+                .unwrap()
+                .to_utc();
 
-//         Self {
-//             start_date: start_date_time,
-//             end_date: end_date_time,
-//         }
-//     }
-// }
+        Self {
+            start_date: start_date_time,
+            end_date: end_date_time,
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
