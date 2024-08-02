3044b0d612f44bfa6c648180cc921cb7bd825bf8
brunbjerg <brunbjerg1423@live.dk>
2024-08-02 19:41:03 +0200

    sync

--- shared_types/src/supervisor/supervisor_scheduling_message.rs
+++ shared_types/src/supervisor/supervisor_scheduling_message.rs
@@ -1,26 +1,19 @@
 use serde::{Deserialize, Serialize};
 
 use crate::scheduling_environment::{
-    work_order::{operation::ActivityNumber, WorkOrderNumber},
-    worker_environment::resources::Id,
+    work_order::WorkOrderActivity, worker_environment::resources::Id,
 };
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct SupervisorSchedulingMessage {
-    pub work_order_number: WorkOrderNumber,
-    pub activity_number: ActivityNumber,
+    pub work_order_activity: WorkOrderActivity,
     pub id_operational: Id,
 }
 
 impl SupervisorSchedulingMessage {
-    pub fn new(
-        work_order_number: WorkOrderNumber,
-        activity_number: ActivityNumber,
-        id_operational: Id,
-    ) -> Self {
+    pub fn new(work_order_activity: WorkOrderActivity, id_operational: Id) -> Self {
         Self {
-            work_order_number,
-            activity_number,
+            work_order_activity,
             id_operational,
         }
     }
