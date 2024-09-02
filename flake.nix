08ac9167f03bf53ecb52b9d81b088708aa009845
cbrje <chri1423@gmail.com>
2024-09-02 16:14:51 +0200

    added: nix flake for reproducible environment

--- flake.nix
+++ flake.nix
@@ -1,15 +1,14 @@
-{ 
-
-  description = "Ordinator Scheduling System";
+{
+  description = "A simple project with a devShell";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }: {
-    flake-utils.lib.eachDefaultSystem = (system:
-      let 
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
         pkgs = import nixpkgs {
           inherit system;
           overlays = [];
@@ -19,14 +18,11 @@
           openpyxl
         ]);
       in {
-        devShell.x86_64-linux.default = pkgs.mkShell {
+        devShells.default = pkgs.mkShell {
           buildInputs = [
             pythonEnv
             pkgs.git
-            pkgs.just
-            pkgs.dust
           ];
         };
       });
-    };
-}
+} 
