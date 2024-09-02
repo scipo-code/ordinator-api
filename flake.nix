ff520fd738c78b34f0c04de2f085c871559766e5
cbrje <chri1423@gmail.com>
2024-09-02 15:52:11 +0200

    sync

--- flake.nix
+++ flake.nix
@@ -4,16 +4,29 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, ... }: {
-    devShell.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.mkShell {
-      buildInputs = [
-        nixpkgs.legacyPackages.x86_64-linux.git
-        nixpkgs.legacyPackages.x86_64-linux.just
-        nixpkgs.legacyPackages.x86_64-linux.dust
-      ];
+  outputs = { self, nixpkgs, flake-utils, ... }: {
+    flake-utils.lib.eachDefaultSystem = (system:
+      let 
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [];
+        };
+        pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+          pandas
+          openpyxl
+        ]);
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pythonEnv
+            nixpkgs.legacyPackages.x86_64-linux.git
+            nixpkgs.legacyPackages.x86_64-linux.just
+            nixpkgs.legacyPackages.x86_64-linux.dust
+          ];
+        };
+      });
     };
-
-  };
 }
