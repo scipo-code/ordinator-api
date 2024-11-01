24c33a9c2b406748ab500d8183eb4113db235b86
cbrje <chri1423@gmail.com>
2024-11-01 16:21:37 +0000

    update: code compiles

--- shared_types/src/supervisor/supervisor_response_status.rs
+++ shared_types/src/supervisor/supervisor_response_status.rs
@@ -4,19 +4,19 @@ use crate::scheduling_environment::worker_environment::resources::Resources;
 
 #[derive(Serialize)]
 pub struct SupervisorResponseStatus {
-    main_work_center: Option<Resources>,
+    supervisor_resource: Vec<Resources>,
     delegated_work_order_activities: usize,
     objective: f64,
 }
 
 impl SupervisorResponseStatus {
     pub fn new(
-        main_work_center: Option<Resources>,
+        main_work_center: Vec<Resources>,
         delegated_work_order_activities: usize,
         objective: f64,
     ) -> Self {
         Self {
-            main_work_center,
+            supervisor_resource: main_work_center,
             delegated_work_order_activities,
             objective,
         }
