c6940712f3664cae7b6b97bf5742681d49b80aec
sebdal <semoda@bio.aau.dk>
2025-02-22 14:02:07 +0000

    updated to tailscaleV4

--- static_files/scheduler/vite.config.ts
+++ static_files/scheduler/vite.config.ts
@@ -1,11 +1,15 @@
 import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/scheduler",
-  plugins: [react()],
+  base: "",
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
