3e5dbf9d93d418edb208b539bdc80ca646e96208
sebdal <semoda@bio.aau.dk>
2025-02-22 16:42:48 +0000

    added export to excel to asset page

--- static_files/scheduler/src/pages/AssetPage.tsx
+++ static_files/scheduler/src/pages/AssetPage.tsx
@@ -1,23 +1,15 @@
 import { Container } from "@/components/Container";
-// import { useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
 import ResourceOverview from "./dashboard/ResourceOverview";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import ExportDialog from "@/components/assetpage/ExportDialog";
 
 export default function AssetPage() {
-  // TODO: Use the url to populate
-  // const { asset } = useParams<{ asset: string}>();
+  const { asset } = useParams<{ asset: string}>();
+
+  if (!asset) {
+    throw new Error("Asset is required");
+  }
 
   return (
     <Container maxWidth="full" padding="sm" className="bg-white min-h-screen">
@@ -39,30 +31,7 @@ export default function AssetPage() {
         </Tabs>
       </div>
       <div className="ml-4">
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button variant="default">Export</Button>
-            </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>Export current workorders to Excel</DialogTitle>
-              <DialogDescription>
-                This will export the current workorders to excel. Working excel disconnects changes from the scheduling system.
-              </DialogDescription>
-            </DialogHeader>
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="filename" className="text-right">
-                Filename
-              </Label>
-              <Input id="filename" value="workorder_schedules.xlsx" className="col-span-3" />
-            </div>
-          </div>
-          <DialogFooter>
-            <Button type="submit">Download</Button>
-          </DialogFooter>
-          </DialogContent>
-        </Dialog>
+        <ExportDialog asset={asset}/>
       </div>
     </div>
       
