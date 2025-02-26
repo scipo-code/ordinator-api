1a91f28295dcb8a57f9b0514ed91f5ec09ace2b5
sebdal <semoda@bio.aau.dk>
2025-02-26 17:56:30 +0000

    Added api resource type

--- static_files/scheduler/src/types.ts
+++ static_files/scheduler/src/types.ts
@@ -1,12 +1,28 @@
 export type Asset = { value: string; label: string };
 
-
 export type WorkCenter = { workCenter: string; noOfTech: number };
-export type ResourcePeriod = { period: string; workCenters: WorkCenter[] }
+export type ResourcePeriod = { period: string; workCenters: WorkCenter[] };
 
 export type ResourceTableRow = {
-  id: string,
-  period: string,
-  [workCenter: string]: number | string
-  
+  id: string;
+  period: string;
+  [workCenter: string]: number | string;
+};
+
+export interface AssetResourceApiResponse {
+  asset: string;
+  metadata: {
+    periods: Array<{
+      id: string;
+      label: string;
+    }>;
+    resources: Array<{
+      id: string;
+      label: string;
+    }>;
+  };
+  data: Array<{
+    periodId: string;
+    values: Record<string, number>;
+  }>;
 }
