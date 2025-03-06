0c9aa3094b426ede6cf70326e739d4844cfb71c0
sebdal <semoda@bio.aau.dk>
2025-03-06 20:41:43 +0000

    Wait for mocker to start in dev

--- static_files/scheduler/src/main.tsx
+++ static_files/scheduler/src/main.tsx
@@ -1,14 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App.tsx'
 import './index.css'
 
-if (import.meta.env.MODE === "development") {
-  import('./mocks/browser.ts').then(({worker}) => worker.start())
+
+async function enableMocking() {
+  if (import.meta.env.MODE !== "development") {
+    return
+  }
+
+  const { worker } = await import('./mocks/browser.ts')
+
+  return worker.start()
 }
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const queryClient = new QueryClient();
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </StrictMode>
+  )
+})
