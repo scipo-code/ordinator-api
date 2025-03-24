a11e144d7616b31477802a3c5e3028b3ff4719da
sebdal <semoda@bio.aau.dk>
2025-03-24 18:48:01 +0000

    added resources endpoint

--- crates/ordinator-api-server/src/routes/api/v1/mod.rs
+++ crates/ordinator-api-server/src/routes/api/v1/mod.rs
@@ -1,9 +1,11 @@
 use actix_web::web;
 
+use crate::handlers::get_asset_resources;
+
 pub fn api_scope() -> actix_web::Scope {
     // Add routes like shown below
     //
-    web::scope("/api")
+    web::scope("/api/v1").route("/{asset}/resources", web::get().to(get_asset_resources))
     // .route(
     //     "/scheduler/export/{asset}",
     //     web::get().to(scheduler_excel_export),
