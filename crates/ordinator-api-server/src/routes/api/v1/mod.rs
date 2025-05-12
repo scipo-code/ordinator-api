56b0efd6775a8ae21049b3ae81d8a3c65621f36a
cbrje <chri1423@gmail.com>
2025-05-12 11:12:54 +0000

    sync

--- crates/ordinator-api-server/src/routes/api/v1/mod.rs
+++ crates/ordinator-api-server/src/routes/api/v1/mod.rs
@@ -5,9 +5,8 @@ use strategic::scheduler_nest;
 
 pub async fn api_scope() -> Router
 {
-    Router::new()
-        .nest("scheduler/", scheduler_nest().await)
-        .nest("/supervisor", router)
+    Router::new().nest("scheduler/", scheduler_nest().await)
+    // .nest("/supervisor", router)
 }
 // pub fn api_scope() -> actix_web::Scope
 // {
